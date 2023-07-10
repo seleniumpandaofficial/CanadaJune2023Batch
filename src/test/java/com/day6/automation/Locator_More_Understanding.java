07561dfaccb382c3abab2756b96858250ee9e15e
seleniumpandaofficial <seleniumpanda@gmail.com>
2023-07-09 21:28:47 -0400

    commiting Day 6 first automotion code

---
+++ src/test/java/com/day6/automation/Locator_More_Understanding.java
@@ -0,0 +1,13 @@
+package com.day6.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locator_More_Understanding {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
